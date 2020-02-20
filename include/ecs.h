362e770aee4eb4aafcbcb93c2843986b55464ae2
Tempo <alexleg33@outlook.fr>
2020-02-20 16:17:48 +0100

    [MODIFIED] add size to buttons

--- include/ecs.h
+++ include/ecs.h
@@ -18,7 +18,7 @@ dg_scene_t *scene_main_menu(void);
 dg_entity_t *ent_music(char *path);
 dg_entity_t *ent_sprite(int id);
 dg_entity_t *ent_text(int x, int y, int scale, char *text);
-dg_entity_t *ent_button(sfVector2f pos, char *text
+dg_entity_t *ent_button(sfVector2f pos, int size, char *text
     , void (*action)(dg_window_t *));
 
 //components
